f52c6f1a6d3546482f221ea0c65bb5804145f03d
donnwilzy <ask4godswill@gmail.com>
2023-01-12 23:19:58 +0100

    initializing

---
+++ db.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+/*
+mongoose.connect('mongodb://donnwilzy:myBTt6KLezNZuIiU@ac-89wqj4p-shard-00-00.alrsosj.mongodb.net:27017,ac-89wqj4p-shard-00-01.alrsosj.mongodb.net:27017,ac-89wqj4p-shard-00-02.alrsosj.mongodb.net:27017/ ?ssl=true&replicaSet=atlas-byur1w-shard-0&authSource=admin&retryWrites=true&w=majority')
+*/
+
+
+
+
+module.exports = db;
---
+++ index.js
@@ -0,0 +1,27 @@
+const express=require('express');
+const app = express();
+const path = ('path');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
+const PORT = 3000;
+const userRoute = require('./routes/user');
+
+dotenv.config()
+
+// mongo db connection
+mongoose
+.connect(process.env.MONGO_URL)
+.then(()=>console.log('db connected successfully'))
+.catch((err)=>{
+    console.log(err);
+});
+
+app.use(express.json());
+app.use('/api/user', userRoute);
+
+
+//local host connection
+app.listen(PORT, ()=>{
+    console.log(`connected to port`)
+})
