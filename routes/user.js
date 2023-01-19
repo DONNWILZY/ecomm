2f96f0f45bfee8af305a69ed3af9a4d681ac683c
donnwilzy <ask4godswill@gmail.com>
2023-01-19 22:59:23 +0100

    tokenization

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,18 @@
+//const { verify } = require('jsonwebtoken');
+const{ verifyToken} = require('./verifyToken') 
+//const {verify} = require('crypto');
 const router = require('express').Router();
-//const router = express.router();
 
+
+//const router = express.router();
+router.put('/:id', verifyToken, verifyTokenAndAuthorization, (req, res)=>{
+    if(req.body.password){
+        req.body.password: CryptoJS.AES.encrypt(
+        req.body.password, 
+        process.env.PASS_SEC
+        ).toString(),
+}
+})
 
 
 
